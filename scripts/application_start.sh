d2fd4e9ab0eb7257a0e3b3d7ce02f3706ac05c85
yungtiec <tctammychu@gmail.com>
2022-12-28 17:34:53 +0800

    load environment vars from aws parameter store

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/hello-world
+
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/hello-world
+
+#install node modules
+sudo yarn
+
+node loadEnv.js
 
 #start our node app in the background
 pm2 start
