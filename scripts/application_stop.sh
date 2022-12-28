d38bb42d5f0b0e23701df05b865c8a89e949e57a
yungtiec <tctammychu@gmail.com>
2022-12-28 12:57:08 +0800

    workflow

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
-pkill node
+
+DIR=/home/ec2-user/hello-world
+if [ -d "$DIR" ] && [ -x "$DIR" ]; then
+  cd /home/ec2-user/hello-world
+
+  # we have to do this because it throws an error if it can't find the process... and then the whole build breaks
+  node -e 'try{require("child_process").execSync("pm2 stop index.js")}catch(e){}';
+fi
