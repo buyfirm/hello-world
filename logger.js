d89fa96a0d6594d2ca14f31e57cda0d2cadce4e5
yungtiec <tctammychu@gmail.com>
2023-01-03 16:34:08 +0800

    logger

--- logger.js
+++ logger.js
@@ -1,28 +1,29 @@
-const winston = require("winston"),
-  WinstonCloudWatch = require("winston-cloudwatch");
-
-const logger = new winston.createLogger({
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.Console({
-      timestamp: true,
-      colorize: true,
-    }),
-  ],
-});
+const winston = require("winston");
+const WinstonCloudWatch = require("winston-cloudwatch");
+if (require.main === module) {
+  const awsParamEnv = require("aws-param-env");
+  awsParamEnv.load("/hello-world/dev", { region: "us-east-2" });
+}
+var cloudwatchConfig;
 
 if (!process.env.IS_LOCALHOST) {
-  const cloudwatchConfig = {
+  console.log(`creating a logger in ${process.env.AWS_REGION}`);
+  cloudwatchConfig = {
     logGroupName: process.env.CLOUDWATCH_GROUP_NAME,
     logStreamName: `${process.env.CLOUDWATCH_GROUP_NAME}--${process.env.NODE_ENV}`,
+    awsRegion: process.env.AWS_REGION,
     awsAccessKeyId: process.env.AWS_ACCESS_KEY,
     awsSecretKey: process.env.AWS_SECRET_KEY,
-    awsRegion: process.env.AWS_REGION,
     messageFormatter: ({ level, message, additionalInfo }) =>
       `[${level}] : ${message} \nAdditional Info: ${JSON.stringify(
         additionalInfo
       )}}`,
   };
-  logger.add(new WinstonCloudWatch(cloudwatchConfig));
+  winston.add(new WinstonCloudWatch(cloudwatchConfig));
+  if (require.main === module)
+    winston.log("info", `Testing hello-world's logger`, {
+      tags: "http",
+    });
 }
-module.exports = logger;
+
+module.exports = winston;
